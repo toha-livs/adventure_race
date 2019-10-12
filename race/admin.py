5c3712b9076b9415b2b8acdcaadbc8390bd71095
toha-livs <aghica@bk.ru>
2019-10-12 23:00:15 +0300

    fot fix

--- race/admin.py
+++ race/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import ControlPoint, CPProtocol, RunGuys, ResultRuns
 
-admin.site.register(CPProtocol)
+
+@admin.register(CPProtocol)
+class CPProtocolAdmin(admin.ModelAdmin):
+    list_filter = ['number']
+
+
+
 admin.site.register(ControlPoint)
 admin.site.register(RunGuys)
 admin.site.register(ResultRuns)
